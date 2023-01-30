aef94e8109e3ac261502da44849d546d23045b10
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2023-01-30 16:19:34 -0500

    added first card data

---
+++ src/Data.js
@@ -0,0 +1,11 @@
+const Data = [
+    {
+        name: "John Smith",
+        cardInfo : {
+            cardType: "VISA",
+            cardNumber: 4485076160182107,
+            date: 2/2025,
+            cvv : 631
+        }
+    }
+]
