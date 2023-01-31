e761226f1106d966c2b16967f0966b94c133371c
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2023-01-31 16:14:11 -0500

    updated data

--- src/Data.js
+++ src/Data.js
@@ -1,47 +1,127 @@
-export const Data = [
-  {
-    name: "John Smith",
-    cardInfo: {
-      cardType: "VISA",
-      cardNumber: 4485076160182107,
-      month: "02",
-      year: "2025",
-      cvv: 631,
-      transactions: [
-        { price: "5", transaction: "starbucks", negative: true },
-        { price: "1,000", transaction: "paycheck", negative: false },
-        { price: "125", transaction: "red sox tickets", negative: true },
-      ],
-    },
+export const Data = {
+  "John Smith" : {
+    cards: [
+      {
+        cardType: "VISA",
+        cardNumber: 4485076160182107,
+        month: "02",
+        year: "2025",
+        cvv: 631,
+        transactions: [
+          { price: "5", transaction: "starbucks", negative: true },
+          { price: "1,000", transaction: "paycheck", negative: false },
+          { price: "125", transaction: "red sox tickets", negative: true },
+        ],
+      },
+      {
+        cardType: "MASTERCARD",
+        cardNumber: 5203906260947058,
+        month: "04",
+        year: "2028",
+        cvv: 183,
+        transactions: [
+          { price: "7.50", transaction: "walgreens", negative: true },
+          { price: "1,800", transaction: "rent", negative: true },
+          { price: "85", transaction: "karate lessons", negative: true },
+        ],
+      },
+      {
+        cardType: "DISCOVER",
+        cardNumber: 6011830616693515,
+        month: "07",
+        year: "2026",
+        cvv: 410,
+        transactions: [
+          { price: "350", transaction: "car payment", negative: true },
+          { price: "120", transaction: "birthday gift", negative: false },
+          { price: "180", transaction: "plumber fees", negative: true },
+        ],
+      },
+    ],
   },
-  {
-    name: "Tim Grossman",
-    cardInfo: {
-      cardType: "MASTERCARD",
-      cardNumber: 5203906260947058,
-      month: "04",
-      year: "2028",
-      cvv: 183,
-      transactions: [
-        { price : "7.50", transaction: "walgreens", negative: true },
-        { price: "1,800", transaction: "rent", negative: true},
-        { price: "85", transaction: "karate lessons", negative : true},
-      ],
-    },
+
+  "Tim Arthur" : {
+    cards: [
+      {
+        cardType: "AMEX",
+        cardNumber: 348426549087847,
+        month: "05",
+        year: "2023",
+        cvv: 268,
+        transactions: [
+          { price: "12", transaction: "pizza", negative: true },
+          { price: "1,400", transaction: "paycheck", negative: false },
+          { price: "80", transaction: "broadway tickets", negative: true },
+        ],
+      },
+      {
+        cardType: "UNIONPAY",
+        cardNumber: 6222026073857579,
+        month: "08",
+        year: "2027",
+        cvv: 732,
+        transactions: [
+          { price: "40", transaction: "gas", negative: true },
+          { price: "30", transaction: "chinese food", negative: true },
+          { price: "100", transaction: "winter jacket", negative: true },
+        ],
+      },
+      {
+        cardType: "VISA",
+        cardNumber: 4485446741380498,
+        month: "06",
+        year: "2024",
+        cvv: 143,
+        transactions: [
+          { price: "120", transaction: "electric bill", negative: true },
+          { price: "120", transaction: "wedding present", negative: false },
+          { price: "1,100", transaction: "new windows", negative: true },
+        ],
+      },
+    ],
   },
-  {
-    name: "Michael Peters",
-    cardInfo: {
-      cardType: "DISCOVER",
-      cardNumber: 6011830616693515,
-      month: "07",
-      year: "2026",
-      cvv: 410,
-      transactions: [
-        { price : "350", transaction : "car payment", negative: true},
-        { price : "120", transaction : "birthday gift", negative: false},
-        { price : "180", transaction : "plumber fees", negative: true}
-      ],
-    },
+
+  "Jack Lewis" : {
+    cards: [
+      {
+        cardType: "DINERS",
+        cardNumber: 3898422938975658,
+        month: "07",
+        year: "2026",
+        cvv: 437,
+        transactions: [
+          { price: "10", transaction: "coffee and donut", negative: true },
+          { price: "2,000", transaction: "rent payments", negative: false },
+          { price: "40", transaction: "ice skating", negative: true },
+        ],
+      },
+      {
+        cardType: "AMEX",
+        cardNumber: 343371115714694,
+        month: "09",
+        year: "2029",
+        cvv: 108,
+        transactions: [
+          { price: "2", transaction: "711 slurpee", negative: true },
+          { price: "60", transaction: "new shirts", negative: true },
+          { price: "50", transaction: "new sneakers", negative: true },
+        ],
+      },
+      {
+        cardType: "VISA",
+        cardNumber: 4408345140263509,
+        month: "07",
+        year: "2026",
+        cvv: 700,
+        transactions: [
+          { price: "160", transaction: "dentist appointment", negative: true },
+          { price: "15", transaction: "new backpack", negative: true },
+          { price: "300", transaction: "graduation gift", negative: false },
+        ],
+      },
+    ],
   },
-];
+
+
+
+};
