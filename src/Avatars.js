846d113e572715bf1842437d2c31cadeae5a262a
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2023-02-03 16:04:15 -0500

    added avatars

--- src/Avatars.js
+++ src/Avatars.js
@@ -3,18 +3,26 @@ import { useSelector } from "react-redux";
 import "./Avatar.css";
 
 const Avatars = () => {
-    const store = useSelector(state => state.Data);
-    const currentUser = useSelector(state => state.Data.currentUser)
-    console.log(store.length)
-  return (
-    <>
-      <img
-        className="avatar"
-        src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1200&q=60
-"
-      />
-    </>
-  );
+  const store = useSelector((state) => state.Data);
+  const currentUser = useSelector((state) => state.Data.currentUser);
+  const keys = Object.keys(store);
+  const names = 
+    keys.filter(
+      (name) =>
+        name !== "currentUser" && name !== "currentCard" && name !== "cardIndex"
+    );
+    return (
+        <div className="images-container">
+            {names.map(name => {
+                return(
+                    <div className="image-container">
+                    <img className="avatar" src={store[name].image} />
+                    <h3>{name}</h3>
+                    </div>
+                )
+            })}
+        </div>
+    ) 
 };
 
 export default Avatars;
