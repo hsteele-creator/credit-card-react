0766f5dc6f460825bd5b129b584fd6ff843a848f
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2023-02-11 19:53:57 -0500

    working on update info page

--- src/UpdateInfo.js
+++ src/UpdateInfo.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./UpdateInfo.css"
 
 const UpdateInfo = () => {
-    return (<h1>Update Info</h1>)
-}
+  const store = useSelector((state) => state.Data);
+  const currrentUser = useSelector((state) => state.Data.currentUser);
+  const image = store[currrentUser].image;
+  return (
+    <>
+      <h1 id="update-info-title">Update Info</h1>
 
-export default UpdateInfo
+      <div id="img-container">
+      <img id="current-user-img" src={image} />
+      </div>
+    </>
+  );
+};
+
+export default UpdateInfo;
