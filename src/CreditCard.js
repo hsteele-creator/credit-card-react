1a7f00580afcac8cbb9576f467636ab208d5ba40
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2023-02-16 22:50:37 -0500

    restyled credit cards

--- src/CreditCard.js
+++ src/CreditCard.js
@@ -24,9 +24,16 @@ const CreditCard = () => {
       card.balnce !== ""
   );
 
+  const spacedCard = [...cards[0].cardNumber]
+    .map((d, i) => (i % 4 == 0 ? " " + d : d))
+    .join("")
+    .trim();
+
+  console.log(spacedCard);
+
   const remove = (cardNumber) => {
     if (filteredCards.length > 1) {
-      dispatch(removeCard(currentUser, cardNumber))
+      dispatch(removeCard(currentUser, cardNumber));
     } else {
       alert("You can't remove all of your cards");
     }
@@ -54,42 +61,48 @@ const CreditCard = () => {
     <>
       <h2 id="card-title">The current user is {currentUser}</h2>
 
-        <div className="container">
-          <Carousel
-            responsive={responsive}
-            afterChange={(previousSlide, { currentSlide, onMove }) => {
-              setCardIndex(
-                currentSlide > previousSlide ? cardIndex + 1 : cardIndex - 1
-              );
-            }}
-          >
-            {filteredCards.map((card, i) => {
-              return (
-                <div key={i} className="card">
-                  <p>{card.cardNumber}</p>
-                  <p>{card.cardType}</p>
-                  <p>{card.year}</p>
-                  <p>{currentUser}</p>
-                  <p>Balance : {card.balance} </p>
-                  <button
-                    id="remove-btn"
-                    onClick={() => remove(card.cardNumber)}
-                  >
-                    x
-                  </button>
-                </div>
-              );
-            })}
-          </Carousel>
+      <div className="container">
+        <Carousel
+          responsive={responsive}
+          afterChange={(previousSlide, { currentSlide, onMove }) => {
+            setCardIndex(
+              currentSlide > previousSlide ? cardIndex + 1 : cardIndex - 1
+            );
+          }}
+        >
+          {filteredCards.map((card, i) => {
+            return (
+              <div key={i} className="card">
+                <p id="card-type">{card.cardType}</p>
+                <p id="credit-card">Credit Card</p>
+                <p id="card-number">
+                  {[...card.cardNumber]
+                    .map((d, i) => (i % 4 == 0 ? " " + d : d))
+                    .join("")
+                    .trim()}
+                </p>
+                <p id="valid">Valid Thru</p>
+                <p id="expiration">{card.month + "/" + card.year}</p>
+                <p id="cvv">CVV</p>
+                <p id="cvv-number">{card.cvv}</p>
+                <button id="remove-btn" onClick={() => remove(card.cardNumber)}>
+                  x
+                </button>
+              </div>
+            );
+          })}
+        </Carousel>
+      </div>
+
+      <NavLink to="/AddCard">
+        <div id="button-container">
+          <Button variant="contained" id="add-card">
+            Add Card
+          </Button>
         </div>
+      </NavLink>
 
-        <NavLink to="/AddCard">
-          <div id="button-container">
-            <Button variant="contained" id="add-card">Add Card</Button>
-          </div>
-        </NavLink>
-
-        <Transaction transactions={transactions} />
+      <Transaction transactions={transactions} />
     </>
   );
 };
